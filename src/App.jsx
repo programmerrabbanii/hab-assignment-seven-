856b03d1477ca028cf163bb96692ccea7b8ec0b8
programmerrabbanii <developerrabbanisarker@gmail.com>
2024-12-03 19:30:11 +0600

    added the footer sectin

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import Allocate from './components/Allocate'
 import Bannar from './components/Bannar'
 import BannarBottom from './components/BannarBottom'
 import Carousel from './components/Carousel'
+import Footer from './components/Footer'
 import Navbar from './components/Navbar'
 import OutFound from './components/OutFound'
 import TrainYour from './components/TrainYour'
@@ -16,7 +17,8 @@ function App() {
      <Carousel></Carousel>
      <Allocate></Allocate>
      <OutFound></OutFound>
-    </>
+     <Footer></Footer>
+    </> 
   ) 
 }
 
--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,7 +1,11 @@
+import footeLogo from "../assets/Logo.png"
 const Footer = () => {
     return (
         <div>
-            
+            <div className="flex justify-between px-6 py-4">
+               <img src={footeLogo} alt="" />
+               <p>Copyright © 2023 Wizia. All rights reserved.</p>
+            </div>
         </div>
     );
 };
